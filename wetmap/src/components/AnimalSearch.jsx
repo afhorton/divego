c07cf9209cce447259f4dcafc6b5635b1f3c2a39
freem11 <freem1985@gmail.com>
2023-01-12 10:24:31 -0700

    dive site selection now sets to state

--- wetmap/src/components/AnimalSearch.jsx
+++ wetmap/src/components/AnimalSearch.jsx
@@ -12,49 +12,49 @@ import { SelectedDiveSiteContext } from "./contexts/selectedDiveSiteContext";
 import { MapBoundsContext } from "./contexts/mapBoundariesContext";
 
 export default function AnimalSearcher() {
-  const { selectedDiveSite, setSelectedDiveSite } = useContext(SelectedDiveSiteContext);
-  const { boundaries, setBoundaries}  = useContext(MapBoundsContext);
+  const { selectedDiveSite, setSelectedDiveSite } = useContext(
+    SelectedDiveSiteContext
+  );
+  const { boundaries, setBoundaries } = useContext(MapBoundsContext);
   const { setShowAnimalSearch } = useContext(AnimalRevealContext);
   const { setAnimalVal } = useContext(AnimalContext);
   const [list, setList] = useState([]);
 
-  let diveSiteNames
-
+  let diveSiteNames;
 
   const handleDiveSiteList = async () => {
-    let diveSiteArray = []
+    let diveSiteArray = [];
 
-    let minLat = boundaries[1]
-    let maxLat = boundaries[3]
+    let minLat = boundaries[1];
+    let maxLat = boundaries[3];
 
-    let minLng = boundaries[0] 
-    let maxLng = boundaries[2]
+    let minLng = boundaries[0];
+    let maxLng = boundaries[2];
 
-    diveSiteNames = null
-    diveSiteArray = []
+    diveSiteNames = null;
+    diveSiteArray = [];
 
-    if (boundaries.length > 0 ){
-    diveSiteNames = await diveSites({minLat, maxLat, minLng, maxLng});  
+    if (boundaries.length > 0) {
+      diveSiteNames = await diveSites({ minLat, maxLat, minLng, maxLng });
     }
 
-    if (diveSiteNames){
-    diveSiteNames.forEach((site) => {
-        if (!diveSiteArray.includes(site.name)){
-          diveSiteArray.push(site.name)
+    if (diveSiteNames) {
+      diveSiteNames.forEach((site) => {
+        if (!diveSiteArray.includes(site.name)) {
+          diveSiteArray.push(site.name);
         }
-    })
-  }
+      });
+    }
 
     setList(diveSiteArray);
+  };
 
-  }
-
-  useEffect(async() => {
-    handleDiveSiteList()
+  useEffect(async () => {
+    handleDiveSiteList();
   }, []);
 
-  useEffect(async() => {
-    handleDiveSiteList()
+  useEffect(async () => {
+    handleDiveSiteList();
   }, [boundaries]);
 
   return (
@@ -63,12 +63,35 @@ export default function AnimalSearcher() {
       id="combo-box-demo"
       options={list}
       onChange={(event, value) => {
-        if (!value) {
-          setAnimalVal("All");
-          setShowAnimalSearch(!setShowAnimalSearch);
-        } else {
-          setAnimalVal(value);
-          setShowAnimalSearch(!setShowAnimalSearch);
+        if (value !== null) {
+          let minLat2 = boundaries[1];
+          let maxLat2 = boundaries[3];
+
+          let minLng2 = boundaries[0];
+          let maxLng2 = boundaries[2];
+
+          let diveSiteSet = diveSites({
+            minLat: minLat2,
+            maxLat: maxLat2,
+            minLng: minLng2,
+            maxLng: maxLng2,
+          });
+
+          Promise.all([diveSiteSet])
+            .then((response) => {
+              response[0].forEach((site) => {
+                if (site.name === value) {
+                  setSelectedDiveSite({
+                    SiteName: site.name,
+                    Latitude: site.lat,
+                    Longitude: site.lng,
+                  });
+                }
+              });
+            })
+            .catch((error) => {
+              console.log(error);
+            });
         }
       }}
       sx={{
