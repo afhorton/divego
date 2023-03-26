7d77ce3e9ec2049f7b4a67f21e1529f87b359664
freem11 <freem1985@gmail.com>
2023-03-25 19:08:15 -0600

    carrousel implemented

--- wetmap/src/components/modals/photoMenu.jsx
+++ wetmap/src/components/modals/photoMenu.jsx
@@ -5,7 +5,11 @@ import { AnimalMultiSelectContext } from "../contexts/animalMultiSelectContext";
 import { useState, useContext, useEffect } from "react";
 import { siteGPSBoundaries } from "../../helpers/mapHelpers";
 import { picClickheatPoints } from "../../supabaseCalls/heatPointSupabaseCalls";
-import { getPhotosforAnchor, getPhotosforAnchorMulti, getPhotosforMapArea } from "../../supabaseCalls/photoSupabaseCalls";
+import {
+  getPhotosforAnchor,
+  getPhotosforAnchorMulti,
+  getPhotosforMapArea,
+} from "../../supabaseCalls/photoSupabaseCalls";
 import { MapBoundsContext } from "../contexts/mapBoundariesContext";
 import { HeatPointsContext } from "../contexts/heatPointsContext";
 // import { getPhotosforAnchor } from "../../axiosCalls/photoAxiosCalls";
@@ -14,6 +18,9 @@ import FlagIcon from "@mui/icons-material/Flag";
 import { Gallery, Item } from "react-photoswipe-gallery";
 import { formatHeatVals } from "../../helpers/heatPointHelpers";
 import "./photoMenu.css";
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+
 
 const PhotoMenu = (props) => {
   const { selectedDiveSite } = useContext(SelectedDiveSiteContext);
@@ -27,7 +34,6 @@ const PhotoMenu = (props) => {
   const [areaPics, setAreaPics] = useState([]);
 
   const filterPhotosForMapArea = async () => {
-
     try {
       const photos = await getPhotosforMapArea({
         minLat: boundaries[1],
@@ -36,7 +42,14 @@ const PhotoMenu = (props) => {
         maxLng: boundaries[2],
       });
       if (photos) {
-        setAreaPics(photos);
+
+        const animalArray = Array.from(
+                  new Set(photos.map((a) => a.label))
+                ).map((label) => {
+                  return photos.find((a) => a.label === label);
+                });
+
+        setAreaPics(animalArray);
       }
     } catch (e) {
       console.log({ title: "Error", message: e.message });
@@ -44,24 +57,25 @@ const PhotoMenu = (props) => {
   };
 
   const filterHeatPointsForMapArea = async (pickedAnimal) => {
-
-    setAnimalVal(pickedAnimal)
+    setAnimalVal(pickedAnimal);
 
     try {
-      const localHeatPoints = await picClickheatPoints({ 
-        minLat: boundaries[1],
-        maxLat: boundaries[3],
-        minLng: boundaries[0],
-        maxLng: boundaries[2],
-      }, pickedAnimal);
+      const localHeatPoints = await picClickheatPoints(
+        {
+          minLat: boundaries[1],
+          maxLat: boundaries[3],
+          minLng: boundaries[0],
+          maxLng: boundaries[2],
+        },
+        pickedAnimal
+      );
       if (localHeatPoints) {
-        setHeatPts(formatHeatVals(localHeatPoints))
+        setHeatPts(formatHeatVals(localHeatPoints));
       }
     } catch (e) {
       console.log({ title: "Error", message: e.message });
     }
   };
-
 
   useEffect(() => {
     filterPhotosForMapArea();
@@ -71,24 +85,44 @@ const PhotoMenu = (props) => {
     filterPhotosForMapArea();
   }, [boundaries]);
 
+  const handleDragStart = (e) => e.preventDefault();
+
   return (
     <div className="masterDiv2">
       <div className="picContainer">
-      <div className="areLabel">Sightings</div>
+        <div className="areLabel">Sightings</div>
       </div>
       <div className="picScoll2">
-        {areaPics &&
+      <AliceCarousel
+      responsive={{
+          0: {items: 1},
+          500: {items: 2},
+          700: {items: 3},
+          900: {items: 4},
+          1100: {items: 5},
+          1300: {items: 6},
+          1500: {items: 7},
+          1700: {items: 8},
+          1900: {items: 9},
+          2100: {items: 10},
+      }}
+        mouseTracking items={areaPics &&
           areaPics.map((pic) => {
             return (
               <div key={pic.id} className="pictureBoxA">
                 <div className="micros">
                   <h4 className="animalLabelArea">{pic.label}</h4>
                 </div>
-                <img 
-                src={`https://lsakqvscxozherlpunqx.supabase.co/storage/v1/object/public/${pic.photoFile}`}
-                width = "192px"
-                height = "108px"
-                onClick={() => filterHeatPointsForMapArea(pic.label)}
+                <img
+                  src={`https://lsakqvscxozherlpunqx.supabase.co/storage/v1/object/public/${pic.photoFile}`}
+                  width="192px"
+                  height="108px"
+                  onDragStart={handleDragStart}
+                  onClick={() => filterHeatPointsForMapArea(pic.label)}
+                  style={{
+                    borderBottomLeftRadius: "10px",
+                    borderBottomRightRadius: "10px",
+                  }}
                 />
                 {/* <Gallery>
                   <div>
@@ -119,6 +153,7 @@ const PhotoMenu = (props) => {
               </div>
             );
           })}
+            />
         {areaPics.length === 0 && (
           <div className="emptySite">
             No Sea Creatures have been added for this area yet.
