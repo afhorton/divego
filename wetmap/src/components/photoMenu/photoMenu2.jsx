187a538008dae1f44bc808b8ce194f6867c6b045
freem11 <freem1985@gmail.com>
2023-07-15 18:08:40 -0600

    pic caddy upgrade working

--- wetmap/src/components/photoMenu/photoMenu2.jsx
+++ wetmap/src/components/photoMenu/photoMenu2.jsx
@@ -1,5 +1,5 @@
 import { AnimalContext } from "../contexts/animalContext";
-import { useState, useContext, useEffect } from "react";
+import { useState, useContext, useEffect, useRef } from "react";
 import { multiHeatPoints } from "../../supabaseCalls/heatPointSupabaseCalls";
 import { getPhotosforMapArea } from "../../supabaseCalls/photoSupabaseCalls";
 import { MapBoundsContext } from "../contexts/mapBoundariesContext";
@@ -8,9 +8,10 @@ import "photoswipe/dist/photoswipe.css";
 import { formatHeatVals } from "../../helpers/heatPointHelpers";
 import "./photoMenu.css";
 import PhotoMenuListItem from "./photoMenuListItem";
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ToggleButton from "@mui/material/ToggleButton";
+import { animated, useSpring } from "react-spring";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 
@@ -110,75 +111,135 @@ const PhotoMenu = () => {
 
   let screenInital = window.innerWidth;
 
-  const [boxWidth, setBoxWidth] = useState(screenInital* 0.99);
-  const [caddyWidth, setCaddyWidth] = useState(Math.floor(screenInital/192)* 193 -192);
+  const [boxWidth, setBoxWidth] = useState(screenInital * 0.99);
+  const [caddyWidth, setCaddyWidth] = useState(
+    Math.floor(screenInital / 192) * 193 - 192
+  );
 
   window.addEventListener("resize", trackWidth);
 
   function trackWidth() {
     setBoxWidth(window.innerWidth * 0.95);
-    setCaddyWidth(Math.floor(window.innerWidth/192)* 193 -192);
+    setCaddyWidth(Math.floor(window.innerWidth / 192) * 193 - 192);
   }
 
-  console.log(areaPics)
+  const wrapperRef = useRef(null);
+  const caddyRef = useRef(null);
+  const leftButtonRef = useRef(null);
+  const rightButtonRef = useRef(null);
+  const [clicked, setClicked] = useState(false);
+
+  const [xCoord, setXCoord] = useState(0);
+
+  const onClicko = (direction) => {
+    let maxLength = areaPics.length * 193;
+
+    console.log("currentX", xCoord);
+    console.log("maxlength", maxLength - caddyWidth);
+    console.log("caddy", caddyWidth);
+    if (direction === "shiftLeft") {
+      if (xCoord >= 0) {
+        setXCoord(0);
+      } else if (xCoord >= maxLength) {
+        setXCoord(maxLength);
+      } else {
+        if (xCoord + caddyWidth < 0) {
+          setXCoord(xCoord + caddyWidth);
+        } else {
+          setXCoord(0)
+        }
+      }
+    } else {
+      //"shiftRight"
+      if (xCoord > 0) {
+        setXCoord(0);
+      } else if (xCoord > -(maxLength - caddyWidth)) {
+        if (xCoord - caddyWidth < -(maxLength - caddyWidth)) {
+          setXCoord(-(maxLength - caddyWidth));
+        } else {
+          setXCoord(xCoord - caddyWidth);
+        }
+      } else {
+        setXCoord(-(maxLength - caddyWidth));
+      }
+    }
+    setClicked(true);
+  };
+
+  const move = useSpring({
+    from: { transform: `translate3d(0,0,0)` },
+    to: { transform: `translate3d(${xCoord}px,0,0)` },
+  });
+
+  useEffect(() => {
+    if (!clicked) return;
+
+    return () => {
+      setClicked(false);
+    };
+  }, [xCoord]);
+
+  useEffect(() => {
+    setXCoord(0);
+    setClicked(false);
+  }, [areaPics.length]);
 
   const toggleButtonStyle = {
     "&.Mui-selected": { backgroundColor: "gold" },
     "&.Mui-selected:hover": { backgroundColor: "gold" },
     backgroundColor: "transparent",
     height: "48px",
-    width:  "48px",
+    width: "48px",
     marginTop: "5px",
     color: "white",
     borderRadius: "100%",
-    }
+  };
+
   return (
-    <div className="masterDivX" style={{width: boxWidth}}>
+    <div className="masterDivX" style={{ width: boxWidth }}>
       <ToggleButton
-      className="backTog"
-          sx={ toggleButtonStyle }
-          // value="check"
-          // selected={showGeoCoder}
-          // onChange={() => {
-          //   console.log("clicked backward")
-          // }}
+        className="backTog"
+        sx={toggleButtonStyle}
+        ref={leftButtonRef}
+        onClick={() => onClicko("shiftLeft")}
+      >
+        <ArrowBackIosIcon />
+      </ToggleButton>
+      <div className="picScollY" style={{ width: caddyWidth }} ref={wrapperRef}>
+        <animated.div
+          className="picScollX"
+          style={({ width: caddyWidth }, move)}
+          ref={caddyRef}
         >
-          <ArrowBackIosIcon />
-        </ToggleButton>
-      <div className="picScollX" style={{width: caddyWidth}}>
-      
-        {areaPics &&
-          areaPics.map((pic) => {
-            return (
-              <PhotoMenuListItem
-                key={pic.id}
-                id={pic.id}
-                name={pic.label}
-                photoURL={pic.photoFile}
-                setAnimalVal={setAnimalVal}
-                animalVal={animalVal}
-              />
-            );
-          })}
-        
-        {areaPics.length === 0 && (
-          <div className="emptyPhotos">
-            No Sea Creatures have been added for this area yet.
-          </div>
-        )}
+          {areaPics &&
+            areaPics.map((pic) => {
+              return (
+                <PhotoMenuListItem
+                  key={pic.id}
+                  id={pic.id}
+                  name={pic.label}
+                  photoURL={pic.photoFile}
+                  setAnimalVal={setAnimalVal}
+                  animalVal={animalVal}
+                />
+              );
+            })}
+
+          {areaPics.length === 0 && (
+            <div className="emptyPhotos">
+              No Sea Creatures have been added for this area yet.
+            </div>
+          )}
+        </animated.div>
       </div>
       <ToggleButton
         className="backTog"
-          sx={ toggleButtonStyle }
-          // value="check"
-          // selected={showGeoCoder}
-          // onChange={() => {
-          //   console.log("clicked forward")
-          // }}
-        >
-          <ArrowForwardIosIcon />
-        </ToggleButton>
-        
+        sx={toggleButtonStyle}
+        ref={rightButtonRef}
+        onClick={() => onClicko("shiftRight")}
+      >
+        <ArrowForwardIosIcon />
+      </ToggleButton>
     </div>
   );
 };
