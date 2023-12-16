b6b18fb3807c5a2b3210642386c78a695ee535cf
afhorton <106110920+afhorton@users.noreply.github.com>
2023-12-16 12:17:04 -0700

    Update express version and configure SSL certificates

--- wetmap/vite.config.js
+++ wetmap/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+require("dotenv"). config({path:`./.env.local`});
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,8 +9,8 @@ export default defineConfig({
       "/api": "http://localhost:5000/",
     },
     https: {
-      key:"/Users/matthewfreeman/key.pem",
-      cert:"/Users/matthewfreeman/cert.pem"
+      key: process.env.VITE_SSL_KEY_FILE,
+      cert: process.env.VITE_SSL_CRT_FILE
     }
   },
   build: {
